73c2d156eea3eae094a57e9488f79abf9df2134f
Le Duy Nguyen <leduy.nguyen@sjsu.edu>
2026-08-29 09:25:47 -0700

    Stop tracking application.properties

---
+++ src/main/java/com/leduynguyen/foodpappspringboot/FoodpAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.leduynguyen.foodpappspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodpAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodpAppSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leduynguyen/foodpappspringboot/FoodpAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leduynguyen.foodpappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodpAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
