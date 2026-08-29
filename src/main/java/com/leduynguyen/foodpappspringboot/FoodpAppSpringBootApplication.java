210472c06137f5e1b8130b968c86d93bc4c62b4a
Le Duy Nguyen <leduy.nguyen@sjsu.edu>
2026-08-29 09:26:51 -0700

    Initialize database, check connection

--- src/main/java/com/leduynguyen/foodpappspringboot/FoodpAppSpringBootApplication.java
+++ src/main/java/com/leduynguyen/foodpappspringboot/FoodpAppSpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoodpAppSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoodpAppSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FoodpAppSpringBootApplication.class, args);
+    }
 
 }
