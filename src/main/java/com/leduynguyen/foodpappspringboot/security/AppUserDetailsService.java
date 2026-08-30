a645bac815108256bfa47da0c0fcac212540648c
Le Duy Nguyen <leduy.nguyen@sjsu.edu>
2026-08-29 23:57:14 -0700

    implements basic security features. Login is required for some paths

---
+++ src/main/java/com/leduynguyen/foodpappspringboot/security/AppUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.leduynguyen.foodpappspringboot.security;
+
+import com.leduynguyen.foodpappspringboot.model.User;
+import com.leduynguyen.foodpappspringboot.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AppUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public AppUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("No account with email " + email));
+        return new AppUserDetails(user);
+    }
+}
